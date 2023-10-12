5a60764e58428060a6321712830882d9fa098cc5
pavithradogiparthi <86222542+pavithradogiparthi@users.noreply.github.com>
2023-10-12 22:25:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/posts", postRoutes);
+app.use("/users", userRoutes);
+
+const PORT = process.env.PORT || 5000;
+const CONNECTION_URL="mongodb+srv://pavithra:1pavithra@cluster0.vqzupdv.mongodb.net/?retryWrites=true&w=majority";
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+// mongoose.set("useFindAndModify", false);
